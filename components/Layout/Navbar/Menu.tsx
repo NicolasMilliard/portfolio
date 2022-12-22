dd3b51fc131a0a49d63cc5df4d8657d622c0858b
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-22 20:05:03 +0100

    Smooth scroll to anchor

--- components/Layout/Navbar/Menu.tsx
+++ components/Layout/Navbar/Menu.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import ButtonLink from '../../Buttons/ButtonLink';
-import Link from 'next/link';
+import ButtonLink from '../../Buttons/ContactLink';
+import { Link } from 'react-scroll';
 
 const Menu = () => {
   return (
     <ul className="flex items-center">
-      <Link href="/#about-me" className="mr-4 sm:mr-8 nm-link">
+      <Link
+        activeClass="nm-active-link "
+        to="about-me"
+        smooth={true}
+        spy={true}
+        offset={-84}
+        duration={300}
+        className="mr-4 cursor-pointer nm-link sm:mr-8"
+      >
         <li>about me</li>
       </Link>
-      <Link href="/#projects" className="mr-4 sm:mr-8 nm-link">
+      <Link
+        activeClass="nm-active-link "
+        to="projects"
+        smooth={true}
+        spy={true}
+        offset={-84}
+        duration={300}
+        className="mr-4 cursor-pointer nm-link sm:mr-8"
+      >
         <li>projects</li>
       </Link>
-      <Link href="#" className="mr-4 sm:mr-8 nm-link">
+      <a href="#" className="mr-4 sm:mr-8 nm-link">
         <li>resume</li>
-      </Link>
+      </a>
       <ButtonLink text="contact me" additionalClasses="" />
     </ul>
   );
