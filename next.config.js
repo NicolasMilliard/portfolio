2fd2f0d6fe07c7823777cbef76288170fe3352e5
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-23 13:43:24 +0100

    Add GOOGLE_TAG_MANAGER env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    GOOGLE_TAG_MANAGER: process.env.GOOGLE_TAG_MANAGER,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
