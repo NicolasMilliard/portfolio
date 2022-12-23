4736842f5b2ca47805749d3e33a84f6fc3b84f28
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-23 13:44:42 +0100

    Add priority false to social links images

--- components/Social/SocialLinks.tsx
+++ components/Social/SocialLinks.tsx
@@ -10,10 +10,10 @@ const SocialLinks = () => {
     <div id="social-links-container" className="fixed hidden lg:block">
       <div className="flex flex-col items-center">
         <Link href="https://github.com/NicolasMilliard" className="mb-4" target="_blank">
-          <Image src={github} alt="Github" className="social-icon" />
+          <Image src={github} alt="Github" className="social-icon" priority={false} />
         </Link>
         <Link href="https://www.linkedin.com/in/nicolas-milliard/" className="mb-4" target="_blank">
-          <Image src={linkedin} alt="LinkedIn" className="social-icon" />
+          <Image src={linkedin} alt="LinkedIn" className="social-icon" priority={false} />
         </Link>
         <div id="social-links-bar"></div>
       </div>
