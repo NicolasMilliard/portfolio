4782cbb451c05864a0a307ca955d496c5bb4a921
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-23 13:44:00 +0100

    Update bg color

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="py-8 bg-black flex items-center justify-center">
+    <footer className="py-8 nm-black-bg-color flex items-center justify-center">
       <p className="nm-white-color">Designed and built by Nicolas&nbsp;Milliard</p>
     </footer>
   );
