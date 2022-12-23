fa96a6d0a7b3d72c0f985a3386b0c83ad2630c08
Nicolas Milliard <contact@nicolasmilliard.fr>
2022-12-23 13:44:24 +0100

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
-import '../styles/globals.css';
+import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import Layout from '../components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){ dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', 'UA-110074739-1');`,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
