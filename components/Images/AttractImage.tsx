4c4bb3c4467f4c9bab1e4ade114dc2dfb5b3f23c
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-01-10 16:56:47 +0100

    Update effect of the image

--- components/Images/AttractImage.tsx
+++ components/Images/AttractImage.tsx
@@ -1,45 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
+import Atropos from 'atropos/react';
 
-import nico from '../../public/images/homepage/nico.png';
+import nico from '../../public/images/homepage/full-stack-developer-nicolas-milliard.png';
 
 const AttractImage = () => {
-  const [imgTop, setImgTop] = useState(0);
-  const [imgLeft, setImgLeft] = useState(0);
-
-  const attractImage = (e: React.MouseEvent) => {
-    let xPos = e.clientX;
-    let yPos = e.clientY;
-
-    setImgTop(yPos);
-    setImgLeft(xPos);
-  };
-
-  const resetImagePos = () => {
-    setImgTop(0);
-    setImgLeft(0);
-  };
-
   return (
-    <div
-      id="image-container"
-      className="hidden lg:block lg:relative lg:cursor-none"
-      onMouseMove={attractImage}
-      onMouseLeave={resetImagePos}
-    >
-      <div
-        id="image-wrapper"
-        className="absolute"
-        style={{ top: `${imgTop / 15}px`, left: `${imgLeft / 65}px` }}
-      >
-        <Image src={nico} alt="Nico" className="rounded-2xl nm-shadow" priority={true} />
-      </div>
-      <div
-        id="image-border"
-        className="absolute"
-        style={{ top: `${imgTop / 25 + 32}px`, left: `${imgLeft / 150 + 32}px` }}
-      ></div>
-    </div>
+    <>
+      <Atropos shadow={false}>
+        <div id="image-container" className="hidden lg:block lg:relative">
+          <div id="image-wrapper">
+            <Image
+              src={nico}
+              alt="Full-Stack Developer - Nicolas Milliard"
+              className="rounded-2xl"
+              priority={true}
+              data-atropos-offset="3"
+            />
+          </div>
+        </div>
+      </Atropos>
+    </>
   );
 };
 
