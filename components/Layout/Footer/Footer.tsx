b5fe6da1ffde2bee2e6ac58ed3cb5f91165f4ebc
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-03-09 12:29:35 +0100

    Update brand identity

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import { useTranslation } from 'next-i18next';
 const Footer = () => {
   const { t } = useTranslation('footer');
   return (
-    <footer className="py-8 nm-black-bg-color flex items-center justify-center">
-      <p className="nm-white-color">{t('footer-copyright')} 🤙 2023</p>
+    <footer className="py-8 bg-brown-900 text-yellow flex items-center justify-center">
+      <p>{t('footer-copyright')} - 2023</p>
     </footer>
   );
 };
