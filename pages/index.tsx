8d720c9437923b6aa73574fbc66df6d8893f53f2
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-03-09 12:29:50 +0100

    Remove ToolsList

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import ContactLink from '../components/Buttons/ContactLink';
 import Separator from '../components/Titles/Separator';
 import AttractImage from '../components/Images/AttractImage';
 import SocialLinks from '../components/Social/SocialLinks';
-import ToolsList from '../components/Projects/ToolsList';
 import ProjectsList from '../components/Projects/ProjectsList';
 
 const Home = () => {
@@ -34,29 +33,32 @@ const Home = () => {
         >
           {/* Text */}
           <div className="mb-20 lg:mb-0 lg:mr-20 xl:mr-40">
-            <h1>{tHomepage('Hello 👋')}</h1>
-            <h2>{tHomepage("I'm Nicolas Milliard")}</h2>
+            <h1 className="font-oswald font-bold text-6xl leading-relaxed text-yellow">
+              {tHomepage('Hello 👋')}
+            </h1>
+            <h2 className="font-oswald font-bold text-5xl leading-relaxed text-yellow">
+              {tHomepage("I'm Nicolas Milliard")}
+            </h2>
             <div className="flex items-center mb-8">
-              <h3 className="text-4xl mr-4 nm-gray-color">{tHomepage('job')}</h3>
-              <Separator additionalClasses="w-72 nm-separator-gray" />
+              <h3 className="font-oswald font-semibold text-4xl leading-relaxed mr-4 text-salmon">
+                {tHomepage('job')}
+              </h3>
+              <Separator additionalClasses="w-60 bg-salmon" />
             </div>
-            <p className="mb-8">
+            <p className="mb-8 text-yellow">
               {tHomepage('about-me-desc-1')}
               <br />
               <Link
                 href="https://certificate.bcdiploma.com/check/B0B491E86C4DD3E9F0065313B4851457AD99C52B28E17896176D6A4E882101BDRlFnSWNWUmJQYVZMZWMvRVZpbTRDdWU1eE9hazdTa0VpMll0ZlVDZVhrSWZjM2pL"
                 target="_blank"
-                className="underline"
+                className="underline hover:text-salmon"
               >
                 {tHomepage("I'm graduated from Alyra")}
               </Link>
               {tHomepage('about-me-desc-2')}
             </p>
-            <ToolsList tools={['React JS', 'React Native', 'Next JS', 'Vue']} />
-            <ToolsList tools={['JavaScript', 'TypeScript', 'HTML5', 'CSS3', 'Solidity']} />
-            <ToolsList tools={['Hardhat', 'Truffle', 'Web3.js', 'Ethers.js']} />
             <div className="mt-12">
-              <ContactLink text={tHomepage('Contact me')} additionalClasses="mr-8" />
+              <ContactLink text={tHomepage('Contact me')} theme="" />
               <Link
                 locale="en"
                 href={
@@ -66,7 +68,7 @@ const Home = () => {
                 }
                 target="_blank"
                 title="Check my resume"
-                className="underline"
+                className="underline text-salmon hover:text-yellow ml-8"
               >
                 {tHomepage('Resume')}
               </Link>
@@ -82,18 +84,22 @@ const Home = () => {
         >
           {/* Title */}
           <div className="flex items-center mb-8">
-            <h2 className="mr-4">{tHomepage('Case studies')}</h2>
-            <Separator additionalClasses="nm-separator-width nm-separator-black" />
+            <h2 className="font-oswald font-bold text-5xl leading-relaxed text-yellow mr-4">
+              {tHomepage('Case studies')}
+            </h2>
+            <Separator additionalClasses="w-72 ml-4 bg-yellow" />
           </div>
           {/* Projects */}
           <ProjectsList />
         </section>
         {/* Get in touch */}
-        <section className="p-8 nm-green-bg-color md:rounded-xl md:max-w-screen-md md:mx-auto md:mb-48">
-          <h2 className="mb-8">{tHomepage('Get in touch!')}</h2>
+        <section className="p-8 bg-yellow text-brown-100 md:rounded-xl md:max-w-screen-md md:mx-auto md:mb-48">
+          <h2 className="font-oswald font-bold text-5xl leading-relaxed mb-8">
+            {tHomepage('Get in touch!')}
+          </h2>
           <p className="mb-4">{tHomepage('contact-me-desc-1')}</p>
           <p className="mb-8">{tHomepage('contact-me-desc-2')}</p>
-          <ContactLink text={tHomepage('Contact me')} additionalClasses={'nm-btn-alt'} />
+          <ContactLink text={tHomepage('Contact me')} theme="salmon" />
         </section>
       </>
     </div>
