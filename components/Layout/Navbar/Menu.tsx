94bd01d4333ce648b82fc5812f0604797d0cf720
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-04-04 20:19:02 +0200

    Menu is now a hamburger on mobile

--- components/Layout/Navbar/Menu.tsx
+++ components/Layout/Navbar/Menu.tsx
@@ -1,59 +1,108 @@
-import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
-
-import france from '../../../public/images/icons/flags/france.svg';
-import usa from '../../../public/images/icons/flags/usa.svg';
 import Button from '../../Buttons/Button';
+
+import github from '../../../public/images/icons/github.svg';
+import twitter from '../../../public/images/icons/twitter.svg';
+import linkedin from '../../../public/images/icons/linkedin.svg';
 
 const Menu = () => {
   const { locale, push } = useRouter();
-
   const { t } = useTranslation('menu');
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (l: string) => () => {
-    push('/', undefined, { locale: l });
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
   return (
-    <ul className="flex items-center">
-      <div className="hidden sm:flex">
-        {locale == 'en' ? (
-          <button className="mr-4 sm:mr-8" onClick={handleClick('fr')}>
-            <Image src={france} alt="France" className="w-8 h-auto" priority={false} />
-          </button>
-        ) : (
-          <button className="mr-4 sm:mr-8" onClick={handleClick('en')}>
-            <Image src={usa} alt="USA" className="w-8 h-auto" priority={false} />
-          </button>
-        )}
-      </div>
-      <Link href="/" className="mr-4 cursor-pointer text-yellow hover:underline sm:mr-8">
-        <li>{t('home')}</li>
-      </Link>
-      <Link href="/casestudies" className="mr-4 cursor-pointer text-yellow hover:underline sm:mr-8">
-        <li>{t('projects')}</li>
-      </Link>
-      <a
-        href={
-          locale == 'en'
-            ? '/images/resume/resume-nicolas-milliard.pdf'
-            : '/images/resume/cv-nicolas-milliard.pdf'
-        }
-        target="_blank"
-        rel="noreferrer"
-        className="mr-4 sm:mr-8 text-yellow hover:underline"
+    <>
+      {/* Hamburger button */}
+      <button className="p-4 flex flex-col items-end gap-1.5 lg:hidden" onClick={toggleMenu}>
+        <div className="block w-7 h-3px bg-black rounded"></div>
+        <div className="block w-5 h-3px bg-black rounded"></div>
+        <div className="block w-7 h-3px bg-black rounded"></div>
+      </button>
+      {/* Menu */}
+      <ul className="hidden lg:flex lg:gap-8">
+        <li>
+          <Link href="#" className="text-black hover:text-green-500">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="#" className="text-black hover:text-green-500">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link href="#" className="text-black hover:text-green-500">
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link href="#" className="text-black hover:text-green-500">
+            Newsletter
+          </Link>
+        </li>
+        <li>
+          <Button text="Contact me" link="#" target="_self" />
+        </li>
+      </ul>
+
+      {/* Floating menu */}
+      <div
+        className={`fixed w-80 py-10 px-8 text-white-500 bg-gradient-200 from-green-100 to-green-900 rounded-bl-3xl ease-in-out duration-300 ${
+          isOpen ? 'right-0 top-0' : '-right-full -top-32'
+        }`}
       >
-        <li>{t('resume')}</li>
-      </a>
-      <Button
-        text={t('contact me')}
-        theme=""
-        link="mailto:contact@nicolasmilliard.fr"
-        target="_self"
-      />
-    </ul>
+        <div className="flex justify-end">
+          <button onClick={toggleMenu} className="relative p-4">
+            <div className="absolute top-4 left-0 block w-7 h-3px bg-white-500 rotate-45 rounded"></div>
+            <div className="absolute top-4 left-0 block w-7 h-3px bg-white-500 -rotate-45 rounded"></div>
+          </button>
+        </div>
+        {/* Portfolio links */}
+        <ul>
+          <li className="mb-4">
+            <Link href="#">Home</Link>
+          </li>
+          <li className="mb-4">
+            <Link href="#">Projects</Link>
+          </li>
+          <li className="mb-4">
+            <Link href="#">Blog</Link>
+          </li>
+          <li>
+            <Link href="#">Newsletter</Link>
+          </li>
+        </ul>
+        <div className="block bg-white-500 w-full h-0.5 my-4 rounded"></div>
+        {/* Social links */}
+        <ul>
+          <li className="flex items-center mb-4">
+            <Image src={github} alt="Github - Nicolas Milliard" className="mr-4" />
+            <Link href="https://github.com/NicolasMilliard" target="_blank">
+              /NicolasMilliard
+            </Link>
+          </li>
+          <li className="flex items-center mb-4">
+            <Image src={twitter} alt="Twitter - Nicolas Milliard" className="mr-4" />
+            <Link href="https://twitter.com/NicolasMilliard" target="_blank">
+              /NicolasMilliard
+            </Link>
+          </li>
+          <li className="flex items-center">
+            <Image src={linkedin} alt="LinkedIn - Nicolas Milliard" className="mr-4" />
+            <Link href="https://www.linkedin.com/in/nicolas-milliard/" target="_blank">
+              /Nicolas-Milliard
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
