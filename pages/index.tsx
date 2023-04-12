00d701cbb42e0e7c2b4535688ce7384b841a968b
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-04-12 18:30:48 +0200

    Update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ import { useTranslation } from 'next-i18next';
 import Introduction from '../components/Homepage/Introduction';
 import AboutMe from '../components/Homepage/AboutMe';
 import Experiences from '../components/Homepage/Experiences';
-import Button from '../components/Buttons/Button';
+import LatestProjects from '../components/Homepage/LatestProjects';
 
 const Home = () => {
   const { t: tHead } = useTranslation('head');
   const { t: tHomepage } = useTranslation('homepage');
 
   return (
-    <div>
+    <>
       <Head>
         <title>{tHead('index-title')}</title>
         <meta
@@ -25,22 +25,9 @@ const Home = () => {
         <Introduction />
         <AboutMe />
         <Experiences />
-
-        {/* Get in touch */}
-        <section className="p-8 bg-yellow text-brown-100 md:rounded-xl md:max-w-screen-md md:mx-auto md:mb-48">
-          <h2 className="font-oswald-semibold text-5xl leading-relaxed mb-8">
-            {tHomepage('Get in touch!')}
-          </h2>
-          <p className="mb-4">{tHomepage('contact-me-desc-1')}</p>
-          <p className="mb-8">{tHomepage('contact-me-desc-2')}</p>
-          <Button
-            text={tHomepage('Contact me')}
-            link="mailto:contact@nicolasmilliard.fr"
-            target="_self"
-          />
-        </section>
+        <LatestProjects />
       </>
-    </div>
+    </>
   );
 };
 
