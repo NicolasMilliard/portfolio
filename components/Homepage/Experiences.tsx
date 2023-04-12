9dda33072d2f62a2ccbddff92bd7c7946fbc6928
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-04-12 18:30:07 +0200

    Update Experiences layout

--- components/Homepage/Experiences.tsx
+++ components/Homepage/Experiences.tsx
@@ -1,45 +1,60 @@
+import { useState } from 'react';
+
+import { experiencesList } from '../../pages/api/experiencesList';
+
 const Experiences = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <section className="mx-6 md:mx-auto mb-32 md:mb-48 sm:px-0 md:px-16 sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-6xl">
       <h2 className="text-3xl text-black font-bold leading-relaxed">Experiences</h2>
-      {/* Experience 1 */}
-      <div>
-        <div className="mt-6 flex gap-6 items-center">
-          <h3 className="text-xl text-green-500 font-semibold leading-relaxed">Freelance</h3>
-          <p>Nov. 2017 - Today</p>
+      {/* Tabs */}
+      <div className="lg:flex lg:gap-20">
+        <div className="hidden lg:block lg:min-w-fit lg:mt-6">
+          {experiencesList.map((experience, index) => (
+            <div
+              key={experience.label}
+              className={`flex gap-2 hover:opacity-100 ease-in-out duration-300 ${
+                activeIndex === index ? 'opacity-100' : 'opacity-40'
+              }`}
+            >
+              <div
+                className={`w-0.5 h-8 ease-in-out duration-300 ${
+                  activeIndex === index ? 'bg-green-500' : 'bg-black'
+                }`}
+              ></div>
+              <button
+                className={`block font-semibold text-left ease-in-out duration-300 ${
+                  activeIndex === index ? 'text-green-500' : 'text-black'
+                }`}
+                onClick={() => setActiveIndex(index)}
+              >
+                {experience.label}
+              </button>
+            </div>
+          ))}
         </div>
-        <ul className="list-disc mt-6 ml-6 text-black leading-relaxed">
-          <li>
-            Develop websites and dApps for many companies (Education, Health, Food, New
-            Technologies...)
-          </li>
-          <li>
-            Implement rich user experiences by creating new features utilizing HTML, CSS, JavaScript
-            and TypeScript
-          </li>
-          <li>Integrate and test new features based on users feedback</li>
-          <li>
-            Development and maintenance of new products, technical documentation and workflows
-          </li>
-        </ul>
-      </div>
-      {/* Experience 2 */}
-      <div>
-        <div className="mt-6 flex gap-6 items-center">
-          <h3 className="text-xl text-green-500 font-semibold leading-relaxed">DMD France</h3>
-          <p>July. 2016 - Nov. 2021</p>
+        {/* Experiences */}
+        <div className="max-w-xl">
+          {experiencesList.map((experience, index) => (
+            <div key={index} className={activeIndex === index ? 'lg:block' : 'lg:hidden'}>
+              <div className="mt-6 flex gap-6 items-center">
+                <h3 className="text-xl text-green-500 font-semibold leading-relaxed lg:hidden">
+                  {experience.label}
+                </h3>
+                <h3 className="text-xl text-green-500 font-semibold leading-relaxed hidden lg:block">
+                  {experience.title}
+                </h3>
+                <p>{experience.date}</p>
+              </div>
+              <ul className="list-disc mt-6 ml-6 text-black leading-relaxed">
+                {experience.descriptions.map((description) => (
+                  <li key={description.id}>{description.text}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
-        <ul className="list-disc mt-6 ml-6 text-black leading-relaxed">
-          <li>Lead architecture, design and development of 15+ websites and applications</li>
-          <li>Develop websites for 4 international business partners</li>
-          <li>Integrate external APIs into web pages and applications</li>
-          <li>Build automated data flows to replace repetitive tasks</li>
-          <li>Integrate and test new features based on users feedback</li>
-          <li>
-            Development and maintenance of new products, technical documentation and workflows
-          </li>
-          <li>Followed industry best practices and learned 3 new technologies</li>
-        </ul>
       </div>
     </section>
   );
