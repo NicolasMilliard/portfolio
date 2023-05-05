cd4fb75f308a623ed47fbdc3813b80cf9edb209d
Nicolas Milliard <contact@nicolasmilliard.fr>
2023-05-05 06:37:47 +0200

    Init commit

---
+++ public/images/case-study/general/index.ts
@@ -0,0 +1,2 @@
+export { default as nextJS } from './nextjs-logo.svg';
+export { default as hardhat } from './hardhat-logo.svg';
--- public/images/case-study/smartvote/index.ts
+++ public/images/case-study/smartvote/index.ts
@@ -3,7 +3,5 @@ export { default as smartVoteBlockchains } from './smart-vote-blockchains.png';
 export { default as smartVoteWhy } from './why-smart-vote.png';
 export { default as smartVoteUXUI } from './ux-ui-smart-vote.png';
 export { default as smartVoteWallets } from './smart-vote-wallets.png';
-export { default as smartVoteNext } from './nextjs-logo.svg';
-export { default as smartVoteHardhat } from './hardhat-logo.svg';
 export { default as smartVoteSolidity } from './solidity-logo.svg';
 export { default as smartVoteEIP } from './smart-vote-eip-1167.png';
